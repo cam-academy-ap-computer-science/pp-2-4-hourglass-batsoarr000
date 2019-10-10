e19b9793ce29a84ea39f3fae0b16ca5e324b260f
batsoarr000 <batsoarr000@CAM-LAB-21.bgsd.k12.wa.us>
2019-10-10 07:53:24 -0700

    Almost

--- src/Hourglass.java
+++ src/Hourglass.java
@@ -2,41 +2,141 @@
 I am hoping to create a for loop system that activates through a constant.
 constant-for loop-for loop-...
 working from inside the hour glass out
+Class constant changes height and levels in the hour glass
 */
 
 public class Hourglass {
-	public static final int TIMECHANGE = 1; 
+	public static final int TIMECHANGE = 1;
 	public static void main(String[] args) {
 		top();
+		middle1();
+		middle2();
+		middle3();
+		middlefa();
+		middle();
+		middlefb();
+		middle6();
+		middle4();
+		middle5();
+		bottom();
 	}
 	public static void top() {
-		for(int i = 1; i >= 1; i++);
-			System.out.println("|\"\"\"\"\"\"\"\"\"\"|");
-			middle1();
-			System.out.println("|\"\"\"\"\"\"\"\"\"\"|");
+		for(int i = 1; i <= 1; i++) {
+			System.out.print("|");
+			for(int j = 1; j <= TIMECHANGE; j++) {
+				System.out.print("\"\"\"\"\"\"\"\"\"\"");
+			}
+		for(int h = 1; h <= 1; h++) {
+			System.out.println("|");
+			}
+		}
 	}
 	public static void middle1() {
-		for(int j = 1; j >= 1; j++);
-			System.out.println(" \\::::::::/");
-			middle2();
-			System.out.println(" /::::::::\\");
+		for(int i = 1; i <= 1; i++) {
+			System.out.print(" \\");
+			for(int j = 1; j <= TIMECHANGE; j++) {
+				System.out.print("::::::::");
+			}
+		for(int h = 1; h <= 1; h++) {
+			System.out.println("/");
+			}
+		}
 	}
 	public static void middle2() {
-		System.out.println("  \\::::::/  ");
-		middle3();
-		System.out.println("  /::::::\\  ");
+		for(int i = 1; i <= 1; i++) {
+			System.out.print("  \\");
+			for(int j = 1; j <= TIMECHANGE; j++) {
+				System.out.print("::::::");
+			}
+		for(int h = 1; h <= 1; h++) {
+			System.out.println("/");
+			}
+		}
 	}
 	public static void middle3() {
-		System.out.println("   \\::::/");
-		middlef();
-		System.out.println("   /::::\\");
-		
+		for(int i = 1; i <= 1; i++) {
+			System.out.print("   \\");
+			for(int j = 1; j <= TIMECHANGE; j++) {
+				System.out.print("::::");
+			}
+		for(int h = 1; h <= 1; h++) {
+			System.out.println("/");
+			}
+		}
 	}
-	public static void middlef() {
-		System.out.println("    \\::/");
-		System.out.println("     ||");
-		System.out.println("    /::\\");
-		
+	public static void middlefa() {
+		for(int i = 1; i <= 1; i++) {
+			System.out.print("    \\");
+			for(int j = 1; j <= TIMECHANGE; j++) {
+				System.out.print("::");
+			}
+		for(int h = 1; h <= 1; h++) {
+			System.out.println("/");
+			}
+		}
 	}
-
+	public static void middle() {
+		for(int i = 1; i <= TIMECHANGE; i++) {
+			System.out.print("     ");
+			for(int j = 1; j <= TIMECHANGE; j++) {
+				System.out.print("||");
+			}
+		System.out.println();
+		}
+	}
+	public static void middlefb() {
+		for(int i = 1; i <= 1; i++) {
+			System.out.print("    /");
+			for(int j = 1; j <= TIMECHANGE; j++) {
+				System.out.print("::");
+			}
+		for(int h = 1; h <= 1; h++) {
+			System.out.println("\\");
+			}
+		}
+	}
+	public static void middle6() {
+		for(int i = 1; i <= 1; i++) {
+			System.out.print("   /");
+			for(int j = 1; j <= TIMECHANGE; j++) {
+				System.out.print("::::");
+			}
+		for(int h = 1; h <= 1; h++) {
+			System.out.println("\\");
+			}
+		}
+	}
+	public static void middle4() {
+		for(int i = 1; i <= 1; i++) {
+			System.out.print("  /");
+			for(int j = 1; j <= TIMECHANGE; j++) {
+				System.out.print("::::::");
+			}
+		for(int h = 1; h <= 1; h++) {
+			System.out.println("\\");
+			}
+		}
+	}
+	public static void middle5() {
+		for(int i = 1; i <= 1; i++) {
+			System.out.print(" /");
+			for(int j = 1; j <= TIMECHANGE; j++) {
+				System.out.print("::::::::");
+			}
+		for(int h = 1; h <= 1; h++) {
+			System.out.println("\\");
+			}
+		}
+	}
+	public static void bottom() {
+		for(int i = 1; i <= 1; i++) {
+			System.out.print("|");
+			for(int j = 1; j <= TIMECHANGE; j++) {
+				System.out.print("\"\"\"\"\"\"\"\"\"\"");
+			}
+		for(int h = 1; h <= 1; h++) {
+			System.out.println("|");
+			}
+		}
+	}
 }
