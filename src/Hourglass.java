71ff01f537ada0d93b0c8d52b587faaa78345173
Arrow Batson <Arrow Batson@LAPTOP-3FEJDBF1>
2019-10-12 19:25:50 -0700

    Finished.  But I have Questions.

--- src/Hourglass.java
+++ src/Hourglass.java
@@ -76,7 +76,7 @@ public class Hourglass {
 		}
 	}
 	public static void middle() {
-		for(int i = 1; i <= TIMECHANGE; i++) {
+		for(int i = 1; i <= 1; i++) {
 			System.out.print("     ");
 			for(int j = 1; j <= TIMECHANGE; j++) {
 				System.out.print("||");
