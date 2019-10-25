001532d884aad90b4ac726f0aa4aeaac484cf91f
batsoarr000 <54676049+batsoarr000@users.noreply.github.com>
2019-10-25 07:56:22 -0700

    Update Hourglass.java

--- src/Hourglass.java
+++ src/Hourglass.java
@@ -3,22 +3,14 @@ I am hoping to create a for loop system that activates through a constant.
 constant-for loop-for loop-...
 working from inside the hour glass out
 Class constant changes height and levels in the hour glass
-
 */
 
 public class Hourglass {
 	public static final int TIMECHANGE = 1;
 	public static void main(String[] args) {
-		top();
-		middle1();
-		middle2();
-		middle3();
-		middlefa();
+		bottom();
+		topup();
 		middle();
-		middlefb();
-		middle6();
-		middle4();
-		middle5();
 		bottom();
 	}
 	public static void top() {
@@ -31,10 +23,11 @@ public class Hourglass {
 			System.out.println("|");
 			}
 		}
-	}
-	public static void middle1() {
+		for(int i = 1; i <= TIMECHANGE; i++) {
+			System.out.print(" ");
+		}
 		for(int i = 1; i <= 1; i++) {
-			System.out.print(" \\");
+			System.out.print("\\");
 			for(int j = 1; j <= TIMECHANGE; j++) {
 				System.out.print("::::::::");
 			}
@@ -42,10 +35,11 @@ public class Hourglass {
 			System.out.println("/");
 			}
 		}
-	}
-	public static void middle2() {
+		for(int i = 1; i <= TIMECHANGE; i++) {
+			System.out.print("  ");
+		}
 		for(int i = 1; i <= 1; i++) {
-			System.out.print("  \\");
+			System.out.print("\\");
 			for(int j = 1; j <= TIMECHANGE; j++) {
 				System.out.print("::::::");
 			}
@@ -53,10 +47,11 @@ public class Hourglass {
 			System.out.println("/");
 			}
 		}
-	}
-	public static void middle3() {
+		for(int i = 1; i <= TIMECHANGE; i++) {
+			System.out.print("   ");
+		}
 		for(int i = 1; i <= 1; i++) {
-			System.out.print("   \\");
+			System.out.print("\\");
 			for(int j = 1; j <= TIMECHANGE; j++) {
 				System.out.print("::::");
 			}
@@ -64,10 +59,11 @@ public class Hourglass {
 			System.out.println("/");
 			}
 		}
-	}
-	public static void middlefa() {
+		for(int i = 1; i <= TIMECHANGE; i++) {
+			System.out.print("    ");
+		}
 		for(int i = 1; i <= 1; i++) {
-			System.out.print("    \\");
+			System.out.print("\\");
 			for(int j = 1; j <= TIMECHANGE; j++) {
 				System.out.print("::");
 			}
@@ -75,19 +71,18 @@ public class Hourglass {
 			System.out.println("/");
 			}
 		}
-	}
-	public static void middle() {
-		for(int i = 1; i <= 1; i++) {
-			System.out.print("     ");
-			for(int j = 1; j <= TIMECHANGE; j++) {
-				System.out.print("||");
-			}
-		System.out.println();
+		for(int i = 1; i <= 5; i++) {
+			System.out.print(" ");
 		}
-	}
-	public static void middlefb() {
+		for(int j = 1; j <= TIMECHANGE; j++) {
+			System.out.print("||");
+			}
+			System.out.println();
+		for(int i = 1; i <= 4; i++) {
+			System.out.print(" ");
+		}
 		for(int i = 1; i <= 1; i++) {
-			System.out.print("    /");
+			System.out.print("/");
 			for(int j = 1; j <= TIMECHANGE; j++) {
 				System.out.print("::");
 			}
@@ -95,10 +90,11 @@ public class Hourglass {
 			System.out.println("\\");
 			}
 		}
-	}
-	public static void middle6() {
+		for(int i = 1; i <= 3; i++) {
+			System.out.print(" ");
+		}
 		for(int i = 1; i <= 1; i++) {
-			System.out.print("   /");
+			System.out.print("/");
 			for(int j = 1; j <= TIMECHANGE; j++) {
 				System.out.print("::::");
 			}
@@ -106,10 +102,11 @@ public class Hourglass {
 			System.out.println("\\");
 			}
 		}
-	}
-	public static void middle4() {
+		for(int i = 1; i <= 2; i++) {
+			System.out.print(" ");
+		}
 		for(int i = 1; i <= 1; i++) {
-			System.out.print("  /");
+			System.out.print("/");
 			for(int j = 1; j <= TIMECHANGE; j++) {
 				System.out.print("::::::");
 			}
@@ -117,10 +114,11 @@ public class Hourglass {
 			System.out.println("\\");
 			}
 		}
-	}
-	public static void middle5() {
+		for(int i = 1; i <= TIMECHANGE; i++) {
+			System.out.print(" ");
+		}
 		for(int i = 1; i <= 1; i++) {
-			System.out.print(" /");
+			System.out.print("/");
 			for(int j = 1; j <= TIMECHANGE; j++) {
 				System.out.print("::::::::");
 			}
@@ -140,4 +138,26 @@ public class Hourglass {
 			}
 		}
 	}
-}
+	public static void topup() {
+		
+        for(int line = 0; line <= 3; line++) {
+                 for(int j = 1; j <= line; j++) {
+                 System.out.print(" ");
+                  }
+                  for(int j = 1; j <= 1; j++) {
+                  System.out.print("\\");
+                  }
+                  for(int j = 0; j <= 7 - 2 * line; j+= 1) {
+                  System.out.print(":");
+                  }
+                  for(int j = 1; j <= 1; j++) {
+                  System.out.print("/");
+                  }
+                  System.out.println();
+                  }
+                  } 
+                  public static void middle() {
+                  System.out.print("    ");
+                  System.out.println("||");
+                  }
+	}
